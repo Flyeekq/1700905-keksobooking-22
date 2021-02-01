5338d136a209fbbad1e20d9f2c3c2c015d4645d8
Max K <flyeekq@gmail.com>
2021-02-01 21:47:45 +0300

    corrected mistakes and notes on on homework #2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  min = Math.abs(min);
+  max = Math.abs(max);
+
+  let tempMin = Math.min(min, max);
+  let tempMax = Math.max(min, max);
+
+  let num = Math.random() * (tempMax - tempMin) + tempMin;
+
+  return Math.round(num)
+}
+
+function getRandomFloatNumber(min, max, precision) {
+  min = Math.abs(min);
+  max = Math.abs(max);
+
+  let tempMin = Math.min(min, max);
+  let tempMax = Math.max(min, max);
+
+  let num = Math.random() * (tempMax - tempMin) + tempMin;
+
+  return num.toFixed(precision);
 }
