bdfeb193068f4916cce7d1c30e6d82bd955a514d
Max K <flyeekq@gmail.com>
2021-02-11 22:33:15 +0300

    split the code into modules, task 4

---
+++ js/data.js
@@ -0,0 +1,43 @@
+const CHEKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHEKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const DESCRIPTIONS = [
+  'просторная светлое помещение',
+  'не очень просторное и темное помещение',
+  'компактно и светло',
+  'большая площадь территории',
+  'просторный двор',
+];
+
+export {CHEKIN, CHEKOUT, FEATURES, PHOTOS, TYPES, DESCRIPTIONS};
